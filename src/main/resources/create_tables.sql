a919652b37b61f06a2e9673f2e782ec3826fac03
Thomas Blanché <thomas.baptiste.blanche@gmail.com>
2025-06-12 11:07:50 +0200

    modif BDD (all FOREIGN KEY ON DELETE)

--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -17,7 +17,7 @@ CREATE TABLE Channel
     id_chan    SERIAL PRIMARY KEY,
     channel    TEXT    NOT NULL,
     subject_id INTEGER NOT NULL,
-    FOREIGN KEY (subject_id) REFERENCES Subject (id_sub)
+    FOREIGN KEY (subject_id) REFERENCES Subject (id_sub) ON DELETE CASCADE
 );
 
 CREATE TABLE Invite
@@ -25,22 +25,22 @@ CREATE TABLE Invite
     invite_id     BIGINT PRIMARY KEY,
     remaining_use INTEGER NOT NULL,
     id_sub        INTEGER NOT NULL,
-    FOREIGN KEY (id_sub) REFERENCES Subject (id_sub)
+    FOREIGN KEY (id_sub) REFERENCES Subject (id_sub) ON DELETE CASCADE
 );
 
 CREATE TABLE Message
 (
-    id_msg             SERIAL PRIMARY KEY,
-    content            TEXT NOT NULL,
-    send_date          timestamp,
-    id_sender  INTEGER,
-    id_receiver            INTEGER,
-    id_channel          INTEGER,
-    response_to        INTEGER,
-    FOREIGN KEY (id_receiver) REFERENCES users (id_user),
-    FOREIGN KEY (id_sender) REFERENCES users (id_user),
-    FOREIGN KEY (id_channel) REFERENCES Channel (id_chan),
-    FOREIGN KEY (response_to) REFERENCES Message (id_msg)
+    id_msg        SERIAL PRIMARY KEY,
+    content       TEXT NOT NULL,
+    send_date     timestamp,
+    id_sender     INTEGER,
+    id_receiver   INTEGER,
+    id_channel    INTEGER,
+    response_to   INTEGER,
+    FOREIGN KEY (id_receiver) REFERENCES users (id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_sender) REFERENCES users (id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_channel) REFERENCES Channel (id_chan) ON DELETE CASCADE,
+    FOREIGN KEY (response_to) REFERENCES Message (id_msg) ON DELETE SET NULL
 );
 
 CREATE TABLE react_to
@@ -49,8 +49,8 @@ CREATE TABLE react_to
     id_msg   INTEGER,
     reaction TEXT,
     PRIMARY KEY (id_user, id_msg),
-    FOREIGN KEY (id_user) REFERENCES users (id_user),
-    FOREIGN KEY (id_msg) REFERENCES Message (id_msg)
+    FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_msg) REFERENCES Message (id_msg) ON DELETE CASCADE
 );
 
 CREATE TABLE user_in_subject
@@ -59,6 +59,6 @@ CREATE TABLE user_in_subject
     id_sub   INTEGER,
     is_admin BOOLEAN,
     PRIMARY KEY (id_user, id_sub),
-    FOREIGN KEY (id_user) REFERENCES users (id_user),
-    FOREIGN KEY (id_sub) REFERENCES Subject (id_sub)
+    FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_sub) REFERENCES Subject (id_sub) ON DELETE CASCADE
 );
